c25f43d35e4e8e3e915ccca331a128a2a4297d95
Joel Guerra <joelaguerra1@gmail.com>
2018-04-02 22:52:58 -0700

    tests and logs

--- heck.py
+++ heck.py
@@ -1,6 +1,25 @@
 import pyspeedtest
+from datetime import datetime
 
 st = pyspeedtest.SpeedTest()
 
-print(st.download())
-print(st.upload())
+
+def get_speeds():
+  speeds = { 
+    "up": str(st.upload() / 1000000),
+    "down": str(st.download() / 1000000),
+  }  
+  return speeds
+
+def log_speeds(speeds):
+  with open("logs.txt", "a+") as f:
+    f.write("At " + str(datetime.now()) + " I got  " + speeds['down'] + " mbs down and " + speeds['up'] + " mbs up.")
+
+
+
+
+
+speeds = get_speeds()
+log_speeds(speeds)
+
+
